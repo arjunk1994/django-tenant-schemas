3243da5b67938919182a6f9582cd4b316be6d1bb
Arjun <arjun@sayonetech.com>
2026-01-21 11:05:31 +0530

    Fixed `--skip-checks` argument conflict in `migrate_schemas` for Django 3.2+.

--- tenant_schemas/management/commands/migrate_schemas.py
+++ tenant_schemas/management/commands/migrate_schemas.py
@@ -1,5 +1,6 @@
 from django.core.management.commands.migrate import Command as MigrateCommand
 from django.db.migrations.exceptions import MigrationSchemaMissing
+
 from tenant_schemas.management.commands import SyncCommon
 from tenant_schemas.migration_executors import get_executor
 from tenant_schemas.utils import (
@@ -15,38 +16,47 @@ class Command(SyncCommon):
     )
 
     def add_arguments(self, parser):
-        super(Command, self).add_arguments(parser)
+        # Add tenant_schemas base arguments
+        super().add_arguments(parser)
+
         command = MigrateCommand()
 
-        # FIX: Allow overriding duplicate arguments (like --skip-checks)
-        # that appear in both BaseCommand and MigrateCommand in Django 3.2+
-        parser.conflict_handler = 'resolve'
+        # 🔥 Django 3.2+ already defines --skip-checks
+        # tenant_schemas tries to add it again → argparse crash
+        # Remove it before re-adding migrate arguments
+        for action in list(parser._actions):
+            if "--skip-checks" in action.option_strings:
+                parser._remove_action(action)
 
+        # Add Django migrate arguments safely
         command.add_arguments(parser)
 
     def handle(self, *args, **options):
-        super(Command, self).handle(*args, **options)
-        self.PUBLIC_SCHEMA_NAME = get_public_schema_name()
+        super().handle(*args, **options)
 
+        self.PUBLIC_SCHEMA_NAME = get_public_schema_name()
         executor = get_executor(codename=self.executor)(self.args, self.options)
 
+        # Public schema
         if self.sync_public and not self.schema_name:
             self.schema_name = self.PUBLIC_SCHEMA_NAME
 
         if self.sync_public:
             executor.run_migrations(tenants=[self.schema_name])
+
+        # Tenant schemas
         if self.sync_tenant:
             if self.schema_name and self.schema_name != self.PUBLIC_SCHEMA_NAME:
                 if not schema_exists(self.schema_name):
                     raise MigrationSchemaMissing(
-                        'Schema "{}" does not exist'.format(self.schema_name)
+                        f'Schema "{self.schema_name}" does not exist'
                     )
-                else:
-                    tenants = [self.schema_name]
+                tenants = [self.schema_name]
             else:
                 tenants = (
                     get_tenant_model()
                     .objects.exclude(schema_name=get_public_schema_name())
                     .values_list("schema_name", flat=True)
                 )
+
             executor.run_migrations(tenants=tenants)
